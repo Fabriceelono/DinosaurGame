9ad26a491b1973a0ffa844b617abb53a21a66986
petervol27 <petervol35@gmail.com>
2024-02-14 21:41:17 +0200

    added quit button jokes

---
+++ menu.js
@@ -0,0 +1,15 @@
+document.getElementById('quit').addEventListener('click', function quit(){
+	alert('Why so scared?');
+	let test = confirm('Sure you wanna be a quitter?');
+	if(test){
+		alert('well quitter, here is the exit...');
+		alert('or is it? muhahahahahhaaaa');
+		window.location.href = './pages/game.html';
+	} else{
+		alert('I knew you were a winner, now press Play!');
+	}
+})
+
+
+
+
